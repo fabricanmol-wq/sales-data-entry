9490122d2bb38cf214eaad57a0470dd4047843b5
Anmolfabric <fabricanmol@gmail.com>
2026-08-03 14:34:08 +0530

    Feat: Remove hardcoded default admins from DataSeeder so only Developer remains after Factory Reset

--- src/main/java/com/salesdata/config/DataSeeder.java
+++ src/main/java/com/salesdata/config/DataSeeder.java
@@ -20,22 +20,6 @@ public class DataSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        if (userRepository.findByUsername("Anmol0001").isEmpty()) {
-            User admin1 = new User();
-            admin1.setUsername("Anmol0001");
-            admin1.setPassword(passwordEncoder.encode("Anmol0001"));
-            admin1.setRole(Role.ADMIN);
-            userRepository.save(admin1);
-        }
-
-        if (userRepository.findByUsername("Salish0001").isEmpty()) {
-            User admin2 = new User();
-            admin2.setUsername("Salish0001");
-            admin2.setPassword(passwordEncoder.encode("Salish0001"));
-            admin2.setRole(Role.ADMIN);
-            userRepository.save(admin2);
-        }
-
         List<User> devUsers = userRepository.findByIsDeveloper(true);
         if (devUsers.isEmpty()) {
             User devAdmin = new User();
