81578276f46580a26ffa10cb4dd57fa907fe4ba3
fabricanmol <fabricanmol@gmail.com>
2026-09-19 13:28:00 +0530

    Clean up unused imports and comments

--- src/main/java/com/salesdata/tasks/KeepAliveTask.java
+++ src/main/java/com/salesdata/tasks/KeepAliveTask.java
@@ -1,6 +1,5 @@
 package com.salesdata.tasks;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
